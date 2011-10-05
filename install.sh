090a7c6fb059fda6d5df07642aa0bd6a46b4dfd8
Marcin Nowicki <pr0d1r2@gmail.com>
2011-10-05 08:38:15 +0200

    Add fix for tarball sources bug.

--- install.sh
+++ install.sh
@@ -82,13 +82,15 @@ cd $MACPORTS_SOURCE_DIR/release/ports || exit $?
 portindex
 
 
-DEFAULT_SOURCES_LINE="rsync://rsync.macports.org/release/ports/ [default]"
+DEFAULT_SOURCES_LINE1="rsync://rsync.macports.org/release/tarballs/ports.tar [default]"
+DEFAULT_SOURCES_LINE2="rsync://rsync.macports.org/release/ports/ [default]"
 SOURCES_CONF_LINE="file://$MACPORTS_SOURCE_DIR/release/ports [nosync]"
 SOURCES_CONF="$MACPORTS_DIR/etc/macports/sources.conf"
 FOUND_IN_SOURCES_CONF=`cat $SOURCES_CONF | fgrep "$SOURCES_CONF_LINE"`
 if [ -z "$FOUND_IN_SOURCES_CONF" ]; then
-  cat $SOURCES_CONF | fgrep -v "$DEFAULT_SOURCES_LINE" > $SOURCES_CONF.tmp
+  cat $SOURCES_CONF | fgrep -v "$DEFAULT_SOURCES_LINE1" | fgrep -v "$DEFAULT_SOURCES_LINE2" > $SOURCES_CONF.tmp
   mv $SOURCES_CONF.tmp $SOURCES_CONF
   echo "$SOURCES_CONF_LINE" >> $SOURCES_CONF
-  echo "$DEFAULT_SOURCES_LINE" >> $SOURCES_CONF
+  echo "$DEFAULT_SOURCES_LINE1" >> $SOURCES_CONF
+  echo "$DEFAULT_SOURCES_LINE2" >> $SOURCES_CONF
 fi
